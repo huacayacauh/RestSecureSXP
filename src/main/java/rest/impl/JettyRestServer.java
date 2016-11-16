f60c65631a7cd5548f858d9bafa359c8fbedf5cd
p16008311 <p16008311@tempo21-2.luminy.univ-amu.fr>
2016-11-16 16:18:04 +0100

    Struct OK

--- src/main/java/rest/impl/JettyRestServer.java
+++ src/main/java/rest/impl/JettyRestServer.java
@@ -1,27 +1,5 @@
 package rest.impl;
 
-/*
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.KeyPairGenerator;
-import java.security.KeyPair;
-import java.security.Security;
-import java.math.BigInteger;
-import java.util.Date;
-
-//certificate
-import javax.security.auth.x500.X500Principal;
-import java.security.cert.X509Certificate;
-
-//Création de la pair de clef (à déplacer pour plus de propreté)
-import org.bouncycastle.x509.extension.SubjectKeyIdentifierStructure;
-import org.bouncycastle.x509.X509V3CertificateGenerator;
-import org.bouncycastle.asn1.x509.X509Extensions;
-import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-//
-*/
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.HttpConnectionFactory;
@@ -171,7 +149,6 @@ public class JettyRestServer implements RestServer{
       }
 	}
 	
-	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -179,91 +156,4 @@ public class JettyRestServer implements RestServer{
 	public void stop() {
 		server.destroy();
 	}
-
 }
-
-/*
-		//////////////////////////////// use deprecated API ///////////////////////////////////////////////////////////
-
-		//Provider custom
-
-		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());								
-
-																													 
-
-		//Mot de passe.																															
-
-		char[] password = {'1', '2', '3', '4', '5', '6'};																				
-
-		KeyStore.PasswordProtection protected_password = new KeyStore.PasswordProtection(password);						
-
-																																						
-
-		//Création du keystore.																													
-
-		KeyStore ks = KeyStore.getInstance("jks");																						
-
-		ks.load(null, password);  //Chargement à partir de rien (creation du keystore et non importation).				
-
-																																						
-
-		//Création de la paire de clef.																										
-
-		KeyPairGenerator key_gen = KeyPairGenerator.getInstance("RSA");															
-
-		key_gen.initialize(1024);																												
-
-		KeyPair keys = key_gen.genKeyPair();																								
-
-																																						
-
-		//Création du certificat.																												
-
-		X509V3CertificateGenerator cert_gen = new X509V3CertificateGenerator();													
-
-																																						
-
-		X500Principal cn = new X500Principal("CN=SXP");																					
-
-		cert_gen.setSerialNumber(new BigInteger("123456789"));																		
-
-		cert_gen.setIssuerDN(cn);																												
-
-		cert_gen.setNotBefore(new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000));									
-
-		cert_gen.setNotAfter(new Date(System.currentTimeMillis() + 365 * 24 * 60 * 60 * 1000));							
-
-		cert_gen.setSubjectDN(cn);																												
-
-		cert_gen.setPublicKey(keys.getPublic());																							
-
-		cert_gen.setSignatureAlgorithm("MD5WithRSA"); //SHA256withRSA																
-
-																																						
-
-																																						
-
-		X509Certificate[] cert_chain = new X509Certificate[1];																		
-
-		cert_chain[0] = cert_gen.generateX509Certificate(keys.getPrivate(), "BC"); //CA private key (autosigned here)		
-																																						
-		ks.setEntry("SXP",																														
-				new KeyStore.PrivateKeyEntry(keys.getPrivate(), cert_chain),														
-				new KeyStore.PasswordProtection(password));
-																																						
-
-		//Enregistement du keystore dans un fichier.																						
-		java.io.FileOutputStream fos = null;																								
-		try 																																			
-		{																																				
-			fos = new java.io.FileOutputStream("keystore.jks");																		
-			ks.store(fos, password);																											
-		}
-		finally
-		{
-			if(fos != null)
-				fos.close();
-		}
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
-		*/
